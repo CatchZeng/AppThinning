edff64513ed68dc2a95895d510149ac84d35e4bd
CatchZeng <891793848@qq.com>
2019-07-25 09:38:46 +0800

    feat: update to 0.2.6

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ middlewareCenter.handleRequest(ctx).then(function(data){
 
 function commander() {
   program
-    .version("0.2.5")
+    .version("0.2.6")
     .option("-d, --dir <String>", "project directory.")
     .option(
       "-t, --types <String>",
